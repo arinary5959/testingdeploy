f7803f2e8ad34e3149181ef2555eb3550857631c
HyeonJeongLee <arinari5959@gmail.com>
2021-04-05 18:32:09 +0900

    008 commit

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-  <hello-world></hello-world>
+  <div>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <hello-world></hello-world>
+  </div>
+  
 </template>
 
 <script>
